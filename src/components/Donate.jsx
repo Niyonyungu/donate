c9ae52462836d87d8a0131001f7a738f1b439ad9
Michel vainqueur <vainqueurmg@gmail.com>
2024-03-22 09:42:52 +0200

    heart

--- src/components/Donate.jsx
+++ src/components/Donate.jsx
@@ -1,59 +1,38 @@
-import React, { useState } from 'react';
+import React, { useState , useEffect } from 'react';
 import donat from "./Donate.module.css";
 
 const Donate = () => {
-  const [donations, setDonations] = useState({ Republicans: 0, Democrats: 0 });
-  const [selectedParty, setSelectedParty] = useState("");
-
-  const handlePartySelection = (event) => {
-    setSelectedParty(event.target.value);
-  };
-
-  const handleDonation = () => {
-    if (selectedParty && donations[selectedParty] !== undefined) {
-      const newDonations = { ...donations };
-      newDonations[selectedParty] += Number(document.getElementById('donationInput').value);
-      setDonations(newDonations);
-    }
-  };
-
-  const handleReset = () => {
-    setDonations({ Republicans: 0, Democrats: 0 });
-  };
-
-  const totalDonation = donations.Republicans + donations.Democrats;
-  const showCenterBubble = donations.Republicans !== 0 && donations.Republicans === donations.Democrats;
 
   return (
     <div className={donat.container}> 
       <div className={donat.parties}>
         <div className={donat.side}>
             <h2 className={donat.rep}>Republicans</h2>
-            <p>{donations.Republicans} <span>$</span></p>
-            <div className={`${donat.bubble} ${donat.bubble1}`} style={{ width: donations.Republicans + 'px', height: donations.Republicans + 'px' }}></div>
-        </div>
-          <div className={donat.center}>
+            <p> 20 <span>$</span></p>
+            {/* <div className={`${donat.bubble} ${donat.bubble1}`}></div> */}
+            <div className={donat.heartr}>❤</div>
+           </div>
+           <div className={donat.center}>
               <h1>Total Donation</h1>
-              <h2><span className={donat.total}>{totalDonation}</span>{""} $ </h2>
-        {showCenterBubble && (
-              <div className={`${donat.bubble} ${donat.bubble3}`} style={{ width: totalDonation + 'px', height: totalDonation + 'px' }}></div>
-              )}
+              <h2><span className={donat.total}>40</span>{""} $ </h2> 
+            {/* <div className={`${donat.bubble} ${donat.bubble3}`}></div> */}
+            <div className={donat.heartc}>❤</div>
           </div>
-        <div className={donat.side}>
+          <div className={donat.side}>
             <h2 className={donat.dem}>Democrats</h2>
-            <p>{donations.Democrats} <span>$</span></p>
-            <div className={`${donat.bubble} ${donat.bubble2}`} style={{ width: donations.Democrats + 'px', height: donations.Democrats + 'px' }}></div>
+            <p> 20<span>$</span></p>
+            {/* <div className={`${donat.bubble} ${donat.bubble2}`}></div> */}
+            <div className={donat.heartd}>❤</div>
         </div>
       </div>
       <div className={donat.donatee}>
-           <select name="" id="" className={donat.sele} onChange={handlePartySelection}>
+           <select name="" id="" className={donat.sele} >
             <option value="">Select Party</option>
             <option value="Democrats">Democrats</option>
             <option value="Republicans">Republicans</option>
            </select>
            <input type="number" id="donationInput" className={donat.inp} placeholder='$' />
-           <button className={donat.dontatebtn} onClick={handleDonation}>Donate</button>
-           <button className={donat.dontatebtnr} onClick={handleReset}>Reset</button>
+           <button className={donat.dontatebtn} >Donate</button>
       </div>
     </div>
   );
